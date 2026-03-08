e68f92be990ad7a5f226ca09cb192806ecfc950c
marius-sys <konto.dla.m.w.q4r@gmail.com>
2026-03-08 10:54:11 +0000

    zmiana url w app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,6 @@
 class HSCodeVerifier {
     constructor() {
-        this.apiBaseUrl = 'hs-code-verifier-api-auth.konto-dla-m-w-q4r.workers.dev'; // nowy worker z auth
+        this.apiBaseUrl = 'hs-code-verifier-api-auth.workers.dev'; // nowy worker z auth
         this.init();
     }
 
